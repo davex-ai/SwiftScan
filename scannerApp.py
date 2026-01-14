a7f911a9e44ed35a296b650004b5422ac2d6694c
davex-ai <omotoyedave4@gmail.com>
2026-01-14 15:15:33 +0100

    ...

--- scannerApp.py
+++ scannerApp.py
@@ -46,3 +46,25 @@ for c in cnts:
 
 print("Document contour found!")
 
+
+def four_point_transform(image, pts):
+    rect = np.array(pts, dtype="float32")
+    (tl, tr, br, bl) = rect
+
+    widthA = np.linalg.norm(br - bl)
+    widthB = np.linalg.norm(tr - tl)
+    maxWidth = max(int(widthA), int(widthB))
+
+    heightA = np.linalg.norm(tr - br)
+    heightB = np.linalg.norm(tl - bl)
+    maxHeight = max(int(heightA), int(heightB))
+
+    dst = np.array([
+        [0, 0],
+        [maxWidth-1, 0],
+        [maxWidth-1, maxHeight-1],
+        [0, maxHeight-1]], dtype="float32")
+
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+    return warped
