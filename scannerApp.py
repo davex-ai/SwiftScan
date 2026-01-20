a730e863c7d61d5f846ce27a60dc7da22fb5e8f6
davex-ai <omotoyedave4@gmail.com>
2026-01-20 03:54:32 +0100

    ...

--- scannerApp.py
+++ scannerApp.py
@@ -2,6 +2,8 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.camera import Camera
 from kivy.uix.button import Button
+from pyzbar import pyzbar
+
 
 class ScannerApp(App):
     def build(self):
@@ -91,3 +93,11 @@ import pytesseract
 text = pytesseract.image_to_string("scanned_final.png")
 print("Detected text:", text)
 
+barcodes = pyzbar.decode(orig)
+
+for barcode in barcodes:
+    data = barcode.data.decode("utf-8")
+    barcode_type = barcode.type
+
+    print(f"[QR] Type: {barcode_type}")
+    print(f"[QR] Data: {data}")
